a142abdd70e787a0bc84090ec5eea70fdd7ad32f
Marlon Derlam <suporte03@sollucao.net>
2026-05-14 14:01:03 +0000

    primeiro site

--- README.md
+++
@@ -1,2 +0,0 @@
-# Sollu-ao
-Site Pessoal
---
+++ script.js
@@ -0,0 +1,9 @@
+// Script simples para o site
+console.log("Site carregado com sucesso!");
+
+function mostrarMensagem() {
+  alert("Bem-vindo ao meu site!");
+}
+
+// Chama a função ao carregar a página
+window.onload = mostrarMensagem;
